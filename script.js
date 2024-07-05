400cd7d85e78e2540f1880a0d21a0f09c5a37e40
Vishnu Madnani <82500052+vishnumadnani@users.noreply.github.com>
2024-07-05 10:18:08 +0530

    Add File from system

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#organizer,#consoles,#game,#type,#selectTime,#sort,#region').select2({
+      placeholder: 'All',
+      allowClear: true,
+      Tags:true
+    });
+  });
+  
